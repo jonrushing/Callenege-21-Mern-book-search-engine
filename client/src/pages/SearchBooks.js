c87e0f41f34b8896101db192131801edacc85104
Jon <Jon@Minettes-MacBook-Pro-2.local>
2023-02-12 14:08:25 -0500

    replaced rest API function with graphql function for saved book

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -5,6 +5,11 @@ import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
 
+//imports for GraphQL
+import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import { useMutation } from '@apollo/client';
+import { SAVE_BOOK } from '../utils/mutations';
+
 const SearchBooks = () => {
   // create state for holding returned google api data
   const [searchedBooks, setSearchedBooks] = useState([]);
@@ -13,6 +18,9 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+
+  //Using Mutation
+  const [saveBook] = useMutation(SAVE_BOOK);
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
@@ -65,7 +73,11 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const response = await saveBook({
+        variables:{
+          input: booksToSave,
+        },
+      });
 
       if (!response.ok) {
         throw new Error('something went wrong!');
