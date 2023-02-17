9777b933355fa8af985ec6169eaba26151943461
Jon <Jon@Minettes-MacBook-Pro-2.local>
2023-02-17 17:58:03 -0500

    searchbook.js works

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -72,13 +72,9 @@ const SearchBooks = () => {
 
     try {
       console.log(bookToSave);
-      const {response} = await saveBook({
+      await saveBook({
         variables:{ bookToStore: bookToSave },
       });
-    
-      if (!response.ok) {  
-        throw new Error('something went wrong!');
-      }
 
       // if book successfully saves to user's account, save book id to state
       setSavedBookIds([...savedBookIds, bookToSave.bookId]);
